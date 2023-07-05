1d1a877c445839e147f20313c1a4da85a04eb03f
root <root@merlin-gpu.partners.org>
2023-07-05 18:51:21 -0400

    add config

---
+++ config.py
@@ -0,0 +1,27 @@
+from pathlib import Path 
+
+PROJECT_DIR = PATH_TO_PROJECT_DIR
+
+# Path to model outputs
+MODEL_RUN_DIR = PROJECT_DIR / 'model_runs/PPH' 
+
+# Path to data folders
+DATA_DIR = PROJECT_DIR / 'data'
+FILTERED_DATA_DIR = DATA_DIR / 'filtered' 
+ANNOTATED_DATA_DIR = DATA_DIR / 'annotated'
+UNLABELLED_DATA_DIR = DATA_DIR / 'unlabelled'
+ANALYSIS_DIR = DATA_DIR / 'analysis'
+RAW_DIR = DATA_DIR / 'raw'
+
+# path to Prancer configs and data
+PRANCER_LABEL_DIR = PROJECT_DIR / 'containerdata/config'
+PRANCER_DATA_DIR = PROJECT_DIR / 'containerdata/data'
+
+# binary & information extraction PPH labels
+binary_labels = [ 'delivery_note', 'Bakri Balloon', 'c-section', 'vaginal - spontaneous vertex', 'laceration', 'uterine atony', 'abruption of the placenta', 
+                    'methergine/methylergometrine', 'manual extraction of placenta', 'PPH', 'placenta accreta spectrum', 'dilation & curettage', 'hysterectomy',
+                     'vaginal - vacuum extraction', 'Uterine rupture',  'cytotec/misoprostol - as uterotonic', 'hemabate/carboprost',
+                      "O'Leary sutures", 'placenta previa', 'disseminated intravascular coagulation (DIC)', 'PPH - surgical causes', 'vaginal - forceps', 
+                      'cryo', 'ffp', 'platelets', 'rbc', 'retained products of conception']
+ie_labels = ['estimated blood loss'] 
+
